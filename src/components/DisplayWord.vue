eae1aed0ac91ef049d7271fd0c498488c4792c94
JayB <jay.bachinicha@proweaver.com>
2021-01-02 21:52:53 +0800

    up

---
+++ src/components/DisplayWord.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="wordContainer">
+    <h1>{{this.newLetter}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DisplayWord',
+  props: ['newLetter']
+}
+</script>
+<style>
+</style>
---
+++ src/components/Timer.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="timer-cont">
+
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Timer',
+    data(){
+        return {
+            
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
